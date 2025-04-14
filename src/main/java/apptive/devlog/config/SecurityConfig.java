ce1a30822c1834b25c407c2757e72af23e371595
doorcs <castledoor@pusan.ac.kr>
2025-04-14 11:41:23 +0900

    feat: 로그인 필터 적용
    * 새로 만든 필터로 기본 로그인 필터를 대체
    * 로그인 필터에서 사용할 의존성 추가
    * 필터를 생성하는 코드를 빌더의 메서드 체인에서 분리
    * 구현체가 아닌 PasswordEncoder 인터페이스를 사용하도록 수정
    * import 구조 수정 (포매터 적용)

--- src/main/java/apptive/devlog/config/SecurityConfig.java
+++ src/main/java/apptive/devlog/config/SecurityConfig.java
@@ -6,44 +6,54 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import apptive.devlog.jwt.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import apptive.devlog.filter.JwtFilter;
+import apptive.devlog.filter.SigninFilter;
+import apptive.devlog.jwt.JwtUtil;
+import apptive.devlog.repository.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
-    public SecurityConfig(JwtUtil jwtUtil) {
-
+    public SecurityConfig(JwtUtil jwtUtil, UserRepository userRepository, ObjectMapper objectMapper) {
         this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
     }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
 
-        http
+        JwtFilter jwtFilter = new JwtFilter(jwtUtil);
+        SigninFilter signinFilter = new SigninFilter(userRepository, passwordEncoder, jwtUtil, objectMapper);
+
+        return http
             .csrf((auth) -> auth.disable())
             .formLogin((auth) -> auth.disable())
             .httpBasic((auth) -> auth.disable())
-            .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
-            // new로 매번 새로 만드는것보다 생성자 주입 방식이나 @Bean 등록 방식을 사용하는 게 더 나을 것 같은데..? 이렇게 쓰는 이유를 이해하고 싶다
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterAt(signinFilter, UsernamePasswordAuthenticationFilter.class) // 기존 UsernamePassword... 필터를 대체
             .authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/", "/signup", "/signin").permitAll()
-                .requestMatchers("/me").hasRole("USER") // 내부적으로 `hasAuthority("ROLE_USER")`로 바뀐다!!
+                .requestMatchers("/me").hasRole("USER")
                 .anyRequest().authenticated())
             .sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
     }
 }
