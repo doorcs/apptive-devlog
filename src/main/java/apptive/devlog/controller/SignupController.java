1eda0975f458853afd23b6a7c4efb46b7709a655
doorcs <castledoor@pusan.ac.kr>
2025-05-20 11:57:55 +0900

    refactor: 버전 관리용 prefix 추가

--- src/main/java/apptive/devlog/controller/SignupController.java
+++ src/main/java/apptive/devlog/controller/SignupController.java
@@ -3,6 +3,7 @@ package apptive.devlog.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import apptive.devlog.dto.SignupDto;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class SignupController {
 
     private final SignupService signupService;
